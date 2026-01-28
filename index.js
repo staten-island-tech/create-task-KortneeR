5e1330731b1cd4179e66a8c8e172cb13089d9bc7
Kortnee Rong <kortneer@nycstudents.net>
2026-01-28 12:32:42 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function RoPaSc (){
+
+}
+    //if a input is put into the terminal
+
+    // computer pick either "rock" "paper" or "scissors"
+
+    //if rock, check if input paper, paper = win, 
+    // scissors = console.log = lose, rock = console.log = tie
+
+    //if paper, check if input scissors, scissors = win, 
+    // rock = console.log = lose, paper = console.log = tie
+
+    //if scissors, check if input rock, rock = win, 
+    // paper = console.log = lose, scissors = console.log = tie
+    
+    //if not rock, not paper, not scissors,
+    //console.log = invalid
