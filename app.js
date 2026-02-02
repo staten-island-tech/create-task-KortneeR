a1958fadb44df42ef3ca3b85b34e9a68e8b2c9c1
Kortnee Rong <kortneer@nycstudents.net>
2026-02-02 00:41:38 -0500

    fesWFESDf

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-const stuff
+document.addEventListener('DOMContentLoaded', () => {
+    const stuff = [
+    "Rock" ,
+    "Paper" ,
+    "Scissors" 
+    ];
+    let history = [];
 
-function RoPaSc (){
-    const btn = document.querySelector(".button");
-    btn.forEach((button)) => {
-        btn.addEventListener('click', (event) => {
-            if ( btn.id === "Rock" && ){
-
-            }
+    function RoPaSc() {
+        let result = "";
+        const historyDiv = document.querySelector('.history');
+        const resultDiv = document.querySelector('.result');
+    
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
         }
-        )
+        
+
+        const btn = document.querySelectorAll(".button");
+
+        btn.forEach((button) => {
+            button.addEventListener("click", () => {
+                const num = stuff[getRandomInt(3)];
+                if (button.id === num) {
+                    result = "Tie";
+                    history.push("Tie");
+                }
+                else if (
+                    (button.id === "Paper" && num === "Rock") ||
+                    (button.id === "Rock" && num === "Scissors") ||
+                    (button.id === "Scissors" && num === "Paper")) {
+                    result = "Win";
+                    history.push("Win");
+                }
+                else {
+                    result = "Lose";
+                    history.push("Lose");
+                }
+                historyDiv.innerHTML = "";
+                for (let i = history.length - 1; i >= 0; i--) {
+                    historyDiv.innerHTML += `<div>Round ${i + 1}: ${history[i]}</div>`;
+                }
+                resultDiv.innerHTML = `<h5>You picked ${button.id} vs Computer picked ${num}: ${result}</h5>`;
+
+            });
+        })
+
     }
 
-}
-    //if a input is put into the terminal
-
-    // computer pick either "rock" "paper" or "scissors"
-
-    //if rock, check if input paper, paper = win, 
-    // scissors = console.log = lose, rock = console.log = tie
-
-    //if paper, check if input scissors, scissors = win, 
-    // rock = console.log = lose, paper = console.log = tie
-
-    //if scissors, check if input rock, rock = win, 
-    // paper = console.log = lose, scissors = console.log = tie
-    
-    //if not rock, not paper, not scissors,
-    //console.log = invalid
+    RoPaSc();
+});
